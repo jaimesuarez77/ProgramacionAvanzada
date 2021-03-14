542b7d9ffd6c161637a55ae153a2cbed4e783f6a
jaimesuarez77 <62628107+jaimesuarez77@users.noreply.github.com>
2021-03-13 19:00:03 -0500

    clase13.03.2021

---
+++ sumarflecha.js
@@ -0,0 +1,11 @@
+function sumarNumeros(numero1,numero2,numero3,numero4){
+    return((numero1+ numero2+numero3+numero4));
+}
+//Funcion flecha
+let sumarNumeros2 =(numero1,numero2,numero3,numero4)=>{
+    return((numero1+ numero2+numero3+numero4));
+}
+
+//ejemplo
+//let sumarNumeros2 =(numero1,numero2,numero3,numero4)=>(numero1+ numero2+numero3+numero4);
+console.log(sumarNumeros2(1,1,1,1));
