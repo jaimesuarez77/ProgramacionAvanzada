19ff5c7d2c7d5d9073a6ac93646934265d87365c
jaimesuarez77 <jaimesuarez77@gmail.com>
2021-03-19 05:20:15 -0500

    modify console
    se modifica el console de temperaturas

--- funcionesFlecha/temperatura.js
+++ funcionesFlecha/temperatura.js
@@ -2,4 +2,4 @@ let temperatura1 = 30;
 let temperatura2 = 25;
 
 let promedioTemperatura = (temperatura1,temperatura2) => (temperatura2+temperatura1)/2;
-    console.log(promedioTemperatura(temperatura1,temperatura2));
+console.log("La temperatura promedio del dia es: "+ promedioTemperatura(temperatura2,temperatura1));
--- funcionesTradicionales/temperatura.js
+++ funcionesTradicionales/temperatura.js
@@ -1,8 +1,9 @@
-let temperatura1 = 30;
+//let temperaturaIni = int(prompt("ingrese la temperatura tomada en la mañana: "));
+let temperatura1 = 26;
 let temperatura2 = 25;
 
 function promedioTemperatura  (temperatura1,temperatura2) {
     return((temperatura2+temperatura1)/2);
 
 } 
-console.log(promedioTemperatura(temperatura1,temperatura2));
+console.log("La temperatura promedio del dia es: "+ promedioTemperatura(temperatura2,temperatura1));
