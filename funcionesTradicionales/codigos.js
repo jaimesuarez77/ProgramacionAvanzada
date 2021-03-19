8cc884d0fa6b13d760e53c5958047bf60aa97982
jaimesuarez77 <jaimesuarez77@gmail.com>
2021-03-18 21:30:29 -0500

    Create codigos.js
    split

---
+++ funcionesTradicionales/codigos.js
@@ -0,0 +1,10 @@
+let separador = ':';
+//let usuario1 = 'ARQ2555 : Sara Bel-sun'.split(separador);
+let usuario = prompt('ingrese su código de nave y su nombre, separado por ":" : '.split(separador));
+let usuario2 = 'ARQ2556 : Nodin chavdri'.split(separador);
+let usuario3 = 'ARQ2557 : Finn'.split(separador);
+
+
+console.log(` Bienvenido capitan ${usuario2[1]}`);
+
+
