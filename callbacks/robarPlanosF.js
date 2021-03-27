c8738830b13775acf4aa984a6a06917b7297204f
jaimesuarez77 <jaimesuarez77@gmail.com>
2021-03-26 19:30:31 -0500

    cambios en el index

--- callbacks/robarPlanosF.js
+++ callbacks/robarPlanosF.js
@@ -3,21 +3,27 @@ let aviso = "El plano robado es el: ";
 let aviso1 = "Operación terminada...";
 let advertencia = "Calleron en la trampa...plano equivocado ";
 let advertencia1 = "Consigan uno valido...";
+let idPlano = prompt("Ingrese el número del plano");
 
 let robarPlanosF = (idPlano,partir)=>{ 
     if(idPlano>0 && idPlano<=10){
-        console.log(aviso,idPlano);
+        
+       // console.log(aviso,idPlano);
+       alert(aviso+idPlano);
         partir(aviso1,null);
     }else{
-        console.log(advertencia);
+        //console.log(advertencia);
+        alert(advertencia);
         partir(null,advertencia1);}}
 
 
-robarPlanosF(12,function(aviso,partir){
+robarPlanosF(idPlano,function(aviso,partir){
     if(aviso){
-        console.log(aviso1);
+        //console.log(aviso1);
+        alert(aviso1);
     }else{
-        console.log(partir)
+        //console.log(partir)
+        alert(partir);
     }
 });
 
