7289871d3b7c178239575c4e6ac25e344e90fc0f
jaimesuarez77 <jaimesuarez77@gmail.com>
2021-03-27 09:36:04 -0500

    change

--- funcionesTradicionales/objeto.js
+++ funcionesTradicionales/objeto.js
@@ -1,24 +1,22 @@
-/*let myCar = new Object();
-myCar.make = 'ford';
-myCar.model = 'Mustang';
-myCar.year = 1969 ;*/
-
-let myCar = {
-    make :'ford',
-    model : 'Mustang',
-    year : 1969 
-
-}
-
-/*
-myCar['make']  = 'Ford';
-myCar['model'] = 'Mustang';
-myCar['year']  = 1969;*/
-
+let fuerza = "El padawan se asigna a las clases del manejo de la fuerza...";
+let sable = "El padawan esta listo para el manejo del 'sable de luz' ...";
+let limite = 15;
 function Padawan( planet, age, height) {
     this.planet = planet;
     this.age = age;
     this.height = height;
   }
-  let Dooku = new Padawan('Serenno', 83, 193);
-  let Cin_Drallig = new Padawan('tierra', 38, 174);
+let Dooku = new Padawan('Serenno', 83, 193);
+let Cin_Drallig = new Padawan('Tierra', 38, 174);
+let Mace_Windy = new Padawan('Haruun Kal', 53, 192);
+let Obi_wan_Kenobi = new Padawan('Stewjon', 12, 182);
+let Luke_Skywalker = new Padawan('Tatooine', 13,172);
+
+//operador ternario
+let asignar = (Obi_wan_Kenobi.age < limite) ? console.log(fuerza) : console.log(sable)
+
+
+//let selection = (escoger)=>{};
+
+
+
